d318f6b8b7c8ba10e1917e8401687245d596c177
Thomas Farstrike <thomasverstreken@protonmail.com>
2022-10-12 06:26:02 +0200

    Jpeg Tiger works

--- src/screen/tft.cpp
+++ src/screen/tft.cpp
@@ -2,6 +2,17 @@
 
 // Include the PNG decoder library
 #include <PNGdec.h>
+
+// JPEG decoder library
+#include <JPEGDecoder.h>
+
+// Return the minimum of two values a and b
+#define minimum(a,b)     (((a) < (b)) ? (a) : (b))
+
+// Include the sketch header file that contains the image stored as an array of bytes
+// More than one image array could be stored in each header file.
+#include "screen/jpeg2.h"
+
 #include "screen/panda.h" // Image is stored here in an 8 bit array
 //#include "screen/bitcoin_200x200.h"
 //#include "screen/bitcoin_logo_240x240.h"
@@ -138,6 +149,126 @@ namespace {
 		png.getLineAsRGB565(pDraw, lineBuffer, PNG_RGB565_BIG_ENDIAN, 0xffffffff);
 		display.pushImage(xpos, ypos + pDraw->y, pDraw->iWidth, 1, lineBuffer);
 	}
+
+	//####################################################################################################
+	// Draw a JPEG on the TFT, images will be cropped on the right/bottom sides if they do not fit
+	//####################################################################################################
+	// This function assumes xpos,ypos is a valid screen coordinate. For convenience images that do not
+	// fit totally on the screen are cropped to the nearest MCU size and may leave right/bottom borders.
+	void renderJPEG(int xpos, int ypos) {
+
+		// retrieve infomration about the image
+		uint16_t *pImg;
+		uint16_t mcu_w = JpegDec.MCUWidth;
+		uint16_t mcu_h = JpegDec.MCUHeight;
+		uint32_t max_x = JpegDec.width;
+		uint32_t max_y = JpegDec.height;
+
+		// Jpeg images are draw as a set of image block (tiles) called Minimum Coding Units (MCUs)
+		// Typically these MCUs are 16x16 pixel blocks
+		// Determine the width and height of the right and bottom edge image blocks
+		uint32_t min_w = minimum(mcu_w, max_x % mcu_w);
+		uint32_t min_h = minimum(mcu_h, max_y % mcu_h);
+
+		// save the current image block size
+		uint32_t win_w = mcu_w;
+		uint32_t win_h = mcu_h;
+
+		// record the current time so we can measure how long it takes to draw an image
+		uint32_t drawTime = millis();
+
+		// save the coordinate of the right and bottom edges to assist image cropping
+		// to the screen size
+		max_x += xpos;
+		max_y += ypos;
+
+		// read each MCU block until there are no more
+		while (JpegDec.readSwappedBytes()) {
+		// save a pointer to the image block
+		pImg = JpegDec.pImage ;
+
+		// calculate where the image block should be drawn on the screen
+		int mcu_x = JpegDec.MCUx * mcu_w + xpos;  // Calculate coordinates of top left corner of current MCU
+		int mcu_y = JpegDec.MCUy * mcu_h + ypos;
+
+		// check if the image block size needs to be changed for the right edge
+		if (mcu_x + mcu_w <= max_x) win_w = mcu_w;
+		else win_w = min_w;
+
+		// check if the image block size needs to be changed for the bottom edge
+		if (mcu_y + mcu_h <= max_y) win_h = mcu_h;
+		else win_h = min_h;
+
+		// copy pixels into a contiguous block
+		if (win_w != mcu_w)
+		{
+		uint16_t *cImg;
+		int p = 0;
+		cImg = pImg + win_w;
+		for (int h = 1; h < win_h; h++)
+		{
+		p += mcu_w;
+		for (int w = 0; w < win_w; w++)
+		{
+		  *cImg = *(pImg + w + p);
+		  cImg++;
+		}
+		}
+		}
+
+		// draw image MCU block only if it will fit on the screen
+		if (( mcu_x + win_w ) <= display.width() && ( mcu_y + win_h ) <= display.height())
+		{
+		display.pushRect(mcu_x, mcu_y, win_w, win_h, pImg);
+		}
+		else if ( (mcu_y + win_h) >= display.height()) JpegDec.abort(); // Image has run off bottom of screen so abort decoding
+		}
+
+		// calculate how long it took to draw the image
+		drawTime = millis() - drawTime;
+
+		// print the results to the serial port
+		Serial.print(F(  "Total render time was    : ")); Serial.print(drawTime); Serial.println(F(" ms"));
+		Serial.println(F(""));
+	}
+
+
+	//####################################################################################################
+	// Print image information to the serial port (optional)
+	//####################################################################################################
+	void jpegInfo() {
+		Serial.println(F("==============="));
+		Serial.println(F("JPEG image info"));
+		Serial.println(F("==============="));
+		Serial.print(F(  "Width      :")); Serial.println(JpegDec.width);
+		Serial.print(F(  "Height     :")); Serial.println(JpegDec.height);
+		Serial.print(F(  "Components :")); Serial.println(JpegDec.comps);
+		Serial.print(F(  "MCU / row  :")); Serial.println(JpegDec.MCUSPerRow);
+		Serial.print(F(  "MCU / col  :")); Serial.println(JpegDec.MCUSPerCol);
+		Serial.print(F(  "Scan type  :")); Serial.println(JpegDec.scanType);
+		Serial.print(F(  "MCU width  :")); Serial.println(JpegDec.MCUWidth);
+		Serial.print(F(  "MCU height :")); Serial.println(JpegDec.MCUHeight);
+		Serial.println(F("==============="));
+	}
+
+	//####################################################################################################
+	// Draw a JPEG on the TFT pulled from a program memory array
+	//####################################################################################################
+	void drawArrayJpeg(const uint8_t arrayname[], uint32_t array_size, int xpos, int ypos) {
+
+		int x = xpos;
+		int y = ypos;
+
+		JpegDec.decodeArray(arrayname, array_size);
+
+		jpegInfo(); // Print information from the JPEG file (could comment this line out)
+
+		renderJPEG(x, y);
+
+		Serial.println("#########################");
+	}
+
+
 }
 
 namespace screen_tft {
@@ -152,6 +283,8 @@ namespace screen_tft {
 	void showInsertFiatScreen(const float &amount) {
 		if (amount == 0.0) {
 			logger::write("Zero amount so showing banner...");
+			drawArrayJpeg(Tiger, sizeof(Tiger), 4, 0); // Draw a jpeg image stored in memory
+			/*
 			//int16_t rc = png.openFLASH((uint8_t *)bitcoin_btc_logo, sizeof(bitcoin_btc_logo), pngDraw);
 			int16_t rc = png.openFLASH((uint8_t *)bitcoin_240x240, sizeof(bitcoin_240x240), pngDraw);
 			//int16_t rc = png.openFLASH((uint8_t *)panda, sizeof(panda), pngDraw);
@@ -167,7 +300,8 @@ namespace screen_tft {
 			} else {
 				Serial.printf("Error with png file: %d\n", rc);
 			}
-			Serial.println("After png file");
+			*/
+			Serial.println("After showing image file");
 		} else {
 			showInsertFiatScreenNonZero(amount);
 		}
